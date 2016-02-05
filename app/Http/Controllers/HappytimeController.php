d81b416953d07eaa2cc7e90adb01390beb2c9618
kunapot <kunapot@KUNAPOTs-MacBook-Pro.local>
2016-02-05 17:40:14 +0700

    add check sent list

--- app/Http/Controllers/HappytimeController.php
+++ app/Http/Controllers/HappytimeController.php
@@ -14,20 +14,26 @@ class HappytimeController extends Controller
     public function store(Request $request)
     {
         $text = $request->input('text');
-        if (empty($text)) {
-            $message = 'ใส่ 1 = sad, 2 = normal, 3 = happy ด้วยนะ';
-        } else if (!preg_match("/^[1-3]$/", $text)) {
-            $message = 'อย่าใส่มั่วซิจ๊ะ เดี๋ยวตบคว่ำเลย';
-        } else {
-            $found = Stat::where('user_id', $request->input('user_id'))
-                        ->whereDate('created_at', '=', Carbon::today()->toDateString())
-                        ->first();
 
-            if (isset($found)) {
-                $message = 'วันนี้ส่งไปแล้วนะ';
+        if ($text == 'list') {
+            $message = Stat::whereDate('created_at', '=', Carbon::today()->toDateString())
+                            ->lists('user_name');
+        } else {
+            if (empty($text)) {
+                $message = 'ใส่ 1 = sad, 2 = normal, 3 = happy ด้วยนะ';
+            } else if (!preg_match("/^[1-3]$/", $text)) {
+                $message = 'อย่าใส่มั่วซิจ๊ะ เดี๋ยวตบคว่ำเลย';
             } else {
-                Stat::create($request->all());
-                $message = 'เรียบร้อยแจร้';
+                $found = Stat::where('user_id', $request->input('user_id'))
+                            ->whereDate('created_at', '=', Carbon::today()->toDateString())
+                            ->first();
+
+                if (isset($found)) {
+                    $message = 'วันนี้ส่งไปแล้วนะ';
+                } else {
+                    Stat::create($request->all());
+                    $message = 'เรียบร้อยแจร้';
+                }
             }
         }
         return $message;
